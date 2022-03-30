9a434481b781fb2a49477b1ed57125fb824d0dfd
João Thome <joao.thome@renovretail.com.br>
2022-03-30 06:31:53 -0300

    :tada: Primeiro commit

---
+++ src/main/java/br/com/integracaodatapoa/IntegracaoDataPoaApplication.java
@@ -0,0 +1,13 @@
+package br.com.integracaodatapoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracaoDataPoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracaoDataPoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/integracaodatapoa/IntegracaoDataPoaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.integracaodatapoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracaoDataPoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
