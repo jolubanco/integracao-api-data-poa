425547f23c41186d209684f81a9d5bb0083769d4
João Thome <joao.thome@renovretail.com.br>
2022-04-04 10:32:14 -0300

    Ajustes estéticos no swagger

--- src/main/java/br/com/integracaodatapoa/controllers/LinhaOnibusController.java
+++ src/main/java/br/com/integracaodatapoa/controllers/LinhaOnibusController.java
@@ -20,6 +20,7 @@ import br.com.integracaodatapoa.controllers.dtos.LinhaOnibusDto;
 import br.com.integracaodatapoa.controllers.forms.LinhaOnibusAtualizarForm;
 import br.com.integracaodatapoa.controllers.forms.LinhaOnibusForm;
 import br.com.integracaodatapoa.services.LinhaOnibusService;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/linhaOnibus")
@@ -28,32 +29,38 @@ public class LinhaOnibusController {
 	@Autowired
 	private LinhaOnibusService linhaOnibusService;
 	
-	@GetMapping//funciona na primeira consulta, na segunda da erro(por causa do like). TIRANDO O LIKE FUNCIONA NORMAL
+	@ApiOperation(value = "Lista todas as linhas de ônibus ou filtra pelo o nome da linha, caso o parâmetro 'nome' seja informado via URL")
+	@GetMapping
 	public ResponseEntity<List<LinhaOnibusDto>> listaLinhasDeOnibus(String nome){
 		return linhaOnibusService.listarLinhasDeOnibus(nome);
 	}
 	
+	@ApiOperation(value = "Exibe a linha de ônibus filtrando pelo ID")
 	@GetMapping("/{idLinha}")
 	public ResponseEntity<LinhaOnibusDto> exibeLinhaDeOnibus(@PathVariable("idLinha") String IdLinha) {
 		return linhaOnibusService.exibirLinhaDeOnibus(IdLinha);
 	}
 	
-	@PostMapping //verifica se existe ou não
-	@Transactional //nao estava atualizando
+	@ApiOperation(value = "Verifica se a linha de ônibus informada existe, caso exista atualiza as informações se não cadastra uma nova linha")
+	@PostMapping
+	@Transactional 
 	public ResponseEntity<LinhaOnibusDto> cadastraLinhaDeOnibus(@RequestBody LinhaOnibusForm form, UriComponentsBuilder uriBuilder) {
 		return linhaOnibusService.cadastrarLinhaOnibus(form,uriBuilder);
 	}
 	
+	@ApiOperation(value = "Consome as informações da Api Data Poa e salva os dados no banco de dados")
 	@PostMapping("/consomeApi")
 	public ResponseEntity<?> consomeApiDataPoa() {
 		return linhaOnibusService.consumirLinhaDeOnibus();	
 	}
 	
+	@ApiOperation(value = "Atualiza os dados da linha de ônibus que foram informados, sendo o 'nome' ou 'código'")
 	@PutMapping
 	public ResponseEntity<LinhaOnibusDto> atualizaLinhaDeOnibus(@RequestBody LinhaOnibusAtualizarForm form) {
 		return linhaOnibusService.atualizarLinhaDeOnibus(form);
 	}
 	
+	@ApiOperation(value = "Deleta uma linha de ônibus a partir do ID")
 	@DeleteMapping("/{idLinha}")
 	public ResponseEntity<?> deletaLinhaDeOnibus(@PathVariable("idLinha") String idLinha){
 		return linhaOnibusService.deletarLinhaDeOnibus(idLinha);
